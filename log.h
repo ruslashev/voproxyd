cb253d94fca97199a6a0b8133f0cb78a7be7d5a7
Ruslan Akbashev <ruslashev@gmail.com>
2018-12-25 19:37:20 +0300

    log: add buffer printing

--- log.h
+++ log.h
@@ -1,12 +1,53 @@
 #pragma once
 
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+
 #ifdef DAEMONIZE
 #include <syslog.h>
 
 #define log(...) syslog(LOG_NOTICE, __VA_ARGS__)
 #else
+#include <stdio.h>
+
 #define log(...) do { printf(__VA_ARGS__); puts(""); } while (0)
 #endif
 
 #define die(X, ...) do { log(__VA_ARGS__); exit(X); } while (0)
+
+static inline int print_byte(char *writebuf, uint8_t x, int base)
+{
+    int len = 8, i = 0;
+
+    if (base == 16) {
+        sprintf(writebuf, "%02x", x);
+        return 2;
+    }
+
+    while (--len >= 0) {
+        writebuf[i++] = (x & ((uint8_t)1 << len)) ? '1' : '0';
+    }
+
+    return 8;
+}
+
+static inline void print_buffer(const void *buf, size_t len, int base)
+{
+    size_t i;
+    int bi = 0;
+    char writebuf[4096] = { 0 };
+
+    if (base != 2 && base != 16)
+        die(1, "print_buffer: base can be 2 or 16, not %d", base);
+
+    for (i = 0; i < len; ++i) {
+        bi += print_byte(writebuf + bi, ((unsigned char*)buf)[i], base);
+        writebuf[bi++] = ' ';
+    }
+
+    writebuf[bi] = 0;
+
+    log("%s", writebuf);
+}
 
