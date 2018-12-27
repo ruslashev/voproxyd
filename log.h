0c36ffc0d219f646a48702c1dd1ec4a710dadead
Ruslan Akbashev <r.akbashev@partner.samsung.com>
2018-12-27 22:47:27 +0300

    compose some messages and allow redefining die()

--- log.h
+++ log.h
@@ -1,20 +1,22 @@
 #pragma once
 
+#include "errors.h"
+
 #include <stddef.h>
 #include <stdint.h>
 #include <stdlib.h>
 
 #ifdef DAEMONIZE
 #include <syslog.h>
-
 #define log(...) syslog(LOG_NOTICE, __VA_ARGS__)
 #else
 #include <stdio.h>
-
 #define log(...) do { printf(__VA_ARGS__); puts(""); } while (0)
 #endif
 
-#define die(X, ...) do { log(__VA_ARGS__); exit(X); } while (0)
+#define die_detail(X, ...) do { log(__VA_ARGS__); exit(X); } while (0)
+
+#define die(X, ...) die_detail(X, __VA_ARGS__)
 
 static inline int print_byte(char *writebuf, uint8_t x, int base)
 {
@@ -39,7 +41,7 @@ static inline void print_buffer(const void *buf, size_t len, int base)
     char writebuf[4096] = { 0 };
 
     if (base != 2 && base != 16)
-        die(1, "print_buffer: base can be 2 or 16, not %d", base);
+        die(ERR_UNSPECIFIED, "print_buffer: base can be 2 or 16, not %d", base);
 
     for (i = 0; i < len; ++i) {
         bi += print_byte(writebuf + bi, ((unsigned char*)buf)[i], base);
@@ -49,5 +51,11 @@ static inline void print_buffer(const void *buf, size_t len, int base)
     writebuf[bi] = 0;
 
     log("%s", writebuf);
+}
+
+static inline void print_buffer_two_bases(const void *buf, size_t len)
+{
+    print_buffer(buf, len, 16);
+    print_buffer(buf, len, 2);
 }
 
--- visca.c
+++ visca.c
@@ -4,12 +4,71 @@
 
 #include <netdb.h>
 
+#define VISCA_OVER_IP_MAX_PAYLOAD_LENGTH 16
+#define VISCA_OVER_IP_HEADER_LENGTH 8
+#define VISCA_OVER_IP_MAX_MESSAGE_LENGTH 24
+
+// redefine die() because all errors here are visca protocol errors
+#undef die
+#define die(...) die_detail(ERR_VISCA_PROTOCOL, __VA_ARGS__)
+
 struct visca_header
 {
     uint16_t payload_type;
     uint16_t payload_length;
     uint32_t seq_number;
-};
+} __attribute__ ((packed));
+
+_Static_assert(sizeof(struct visca_header) == VISCA_OVER_IP_HEADER_LENGTH,
+        "VISCA header size must be 8 bytes");
+
+static uint32_t g_seq_number = 0;
+
+static void compose_ack(uint8_t *buffer, size_t *n)
+{
+    *(buffer + 0) = 0x90;
+    *(buffer + 1) = 0x40;
+    *(buffer + 2) = 0xff;
+
+    *n = 3;
+}
+
+static void compose_completition(uint8_t *buffer, size_t *n, uint8_t data[], size_t data_len)
+{
+    *(buffer + 0) = 0x90;
+    *(buffer + 1) = 0x50;
+
+    for (size_t i = 0; i < data_len; ++i) {
+        *(buffer + 2 + i) = data[i];
+    }
+
+    *(buffer + 2 + data_len) = 0xff;
+
+    *n = 3 + data_len;
+}
+
+static void compose_empty_completition(uint8_t *buffer, size_t *n)
+{
+    compose_completition(buffer, n, NULL, 0);
+}
+
+static void compose_control_reply(uint8_t *buffer, size_t *n)
+{
+    struct visca_header header = {
+        .payload_type = 0x0201,
+        .payload_length = 0x01,
+        .seq_number = g_seq_number++ // todo: packet may be missed
+    };
+
+    buffer = (uint8_t*)&header;
+
+    *(buffer + VISCA_OVER_IP_HEADER_LENGTH) = 0x01; /* ACK: reply for RESET */
+
+    *n = VISCA_OVER_IP_HEADER_LENGTH + 1;
+
+    log("compose_control_reply: debug response:");
+    print_buffer_two_bases(buffer, *n);
+}
 
 static void visca_header_convert_endianness(struct visca_header *header)
 {
@@ -64,13 +123,22 @@ static void handle_control_command(const uint8_t *payload, size_t length, uint32
 
     switch (payload[0]) {
         case 0x01: /* RESET */
-            log("visca control command RESET");
+            log("control command RESET");
             break;
         case 0x0F: /* ERROR */
             if (length != 2) {
-                die(ERR_PROTOCOL, "handle_control_command: ERROR: excepted length == 2, got %ld",
-                        length);
+                die("handle_control_command: ERROR: excepted length == 2, got %ld", length);
             }
+            switch (payload[1]) {
+                case 0x01:
+                    log("abnormality in the sequence number");
+                    break;
+                case 0x02:
+                    log("abnormality in the sequence number");
+                    break;
+            }
+        default:
+            die("handle_control_command: unexpected control command type 0x%x", payload[0]);
     }
 }
 
@@ -101,8 +169,7 @@ void visca_handle_message(const uint8_t *message, size_t length)
     log("header->seq_number=%d", header->seq_number);
 
     if (header->payload_length != length - 8) {
-        die(ERR_PROTOCOL,
-                "assertion `header->payload_length == length - 8' failed: %d != %ld",
+        die("assertion `header->payload_length == length - 8' failed: %d != %ld",
                 header->payload_length, length - 8);
     }
 
